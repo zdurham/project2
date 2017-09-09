12c23f9ac488129d8fcba17d71a58b4eb9f1b862
Zach Durham <ZachDurham@dhcp-v80-000602.wireless.unc.edu>
2017-09-09 13:31:10 -0400

    demonstrated ability to display all posts on pug template

--- routes/post-routes.js
+++ routes/post-routes.js
@@ -2,16 +2,12 @@ const db = require("../models")
 
 module.exports = (app) => {
 
+  // Renders the create-post template
   app.get("/create-post", (req, res) => {
     res.render('create-post')
   })
 
-  app.get("/api/posts", (req, res) => {
-    db.Post.findAll({}).then((results) => {
-      res.json(results);
-    })
-  })
-
+  // Puts posts into DB and links them to the user that is logged in
   app.post('/create-post', (req, res) => {
     db.Post.create({
       title: req.body.title,
@@ -19,6 +15,17 @@ module.exports = (app) => {
       body: req.body.body,
       image: req.body.image ? req.body.image : null,
       UserId: req.user.id
+    })
+  })
+
+  //---------------------------------------------
+  // Api route below
+  //---------------------------------------------
+
+  // Gets posts in json format
+  app.get("/api/posts", (req, res) => {
+    db.Post.findAll({}).then((results) => {
+      res.json(results);
     })
   })
 }
--- routes/routes.js
+++ routes/routes.js
@@ -1,12 +1,20 @@
 const db = require("../models")
 
 module.exports = (app) => {
-  app.get("/", (req, res) => {
+  
+  // Displays the front page
+  app.get('/', (req, res) => {
     res.render("front")
   })
 
+  app.get('/post-list', (req, res) => {
+    db.Post.findAll().then(dbPost => {
+      res.render('post-list', {posts: dbPost})
+    })    
+  })
+
   // Display all Users and their related comments and posts
-  app.get("/api/users", (req, res) => {
+  app.get('/api/users', (req, res) => {
     db.User.findAll({ include: [{model: db.Post}, {model: db.Comment}]}).then(dbUser => {
       res.json(dbUser)
     })
